85780c59f610dd01a1c4227c21abdfebd0d8a0e1
aggielandwebco <elijahdaniel@aggielandwebco.com>
2026-05-08 23:49:37 -0500

    Update AuthContext.jsx

--- src/lib/AuthContext.jsx
+++ src/lib/AuthContext.jsx
@@ -1,15 +1,26 @@
 import { createContext, useContext } from "react";
 
-const AuthContext = createContext();
+const AuthContext = createContext({
+  isAuthenticated: true,
+  isLoadingAuth: false,
+  isLoadingPublicSettings: false,
+  authChecked: true,
+  authError: null,
+  navigateToLogin: () => {},
+  checkUserAuth: () => {}
+});
 
 export function AuthProvider({ children }) {
   return (
     <AuthContext.Provider
       value={{
+        isAuthenticated: true,
         isLoadingAuth: false,
         isLoadingPublicSettings: false,
+        authChecked: true,
         authError: null,
-        navigateToLogin: () => {}
+        navigateToLogin: () => {},
+        checkUserAuth: () => {}
       }}
     >
       {children}
